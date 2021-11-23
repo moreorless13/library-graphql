a5c83d3d4e2481ba7f8e46dce961c3af0bc27776
James Strande <strandejr@gmail.com>
2021-11-23 18:57:08 -0500

    backend converted to use graphql

--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,7 @@
 const User = require('./User');
+const bookSchema = require('./Book');
 
-module.exports = { User };
+module.exports = { 
+    User, 
+    bookSchema 
+};
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,49 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String!
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Book {
+        bookId: ID!
+        authors: [String]
+        description: String
+        image: String
+        link: String
+        title: String!
+    }
+
+    type Auth {
+        token: ID!
+        User: User
+    }
+
+    type BookInput {
+        authors: [String]
+        description: String!
+        bookId: String!
+        image: String
+        link: String
+        title: String!
+    }
+
+    type Query {
+        users: [User]!
+        user(userId: ID!): User
+        me: User
+    }
+
+    type Mutation {
+        createUser(username: String!, email: String!, password: String!): Auth
+        login(username: String!, password: String!): Auth
+        saveBook(bookData: BookInput!): User
+        removeBook(bookId: ID!): User
+    }
+`;
+
+module.exports = typeDefs;
